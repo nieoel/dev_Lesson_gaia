197792f5dc2f235dc46bff5dcc0290890e06ec90
nieoel <yts1220@gmail.com>
2017-11-28 17:29:11 +0900

    landingpage/1128_Tue

--- landingpage/1127_Mon/js/custom.js
+++ landingpage/1127_Mon/js/custom.js
@@ -1,6 +1,47 @@
-
-
-
+/**********************************************
+                wow
+*********************************************/
 $(function() {
   // Handler for .ready() called.
+    new WOW().init();
+
+});
+
+/**********************************************
+                magnific popup
+    http://dimsemenov.com/plugins/magnific-popup/documentation.html#initializing-popup
+*********************************************/
+$(function() {
+$('#work').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  gallery:{
+    enabled: true
+    }
+  // other options
+});
+});
+
+/*
+$('.gallery').each(function(){
+  $(this).magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',
+  gallery:{
+    enabled: true
+    }
+  // other options
+});
+});
+*/
+
+
+/**********************************************
+                magnific popup
+    http://dimsemenov.com/plugins/magnific-popup/documentation.html#initializing-popup
+*********************************************/
+
+$(function(){
+  $(".owl-carousel").owlCarousel();
+  items : 3;
 });
